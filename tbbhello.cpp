3645988a65386606336419c7d1dbbdbf257c8812
2333luo <2454157917@qq.com>
2022-06-24 11:17:20 +0800

    one  diff test

--- tbbhello.cpp
+++ tbbhello.cpp
@@ -5,6 +5,7 @@ int main(){
     tbb::parallel_invoke(
         [](){ std::cout<<"Hello "<<std::endl; },
         [](){ std::cout<<"TBB!"<<std::endl; }
+        printf("one changed")
     );
     return 0;
 }
